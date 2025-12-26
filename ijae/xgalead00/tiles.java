9cf7bb09870ccb4ed9b157de414ec90866c890bc
Dave Galea <davegalea27@gmail.com>
2025-12-26 15:11:13 +0100

    tiles definition + test assets

---
+++ ijae/xgalead00/tiles.java
@@ -0,0 +1,23 @@
+package ijae.xgalead00
+
+public enum TileType {
+	WALL("wall.png"),
+	GATE("gate.png"),
+	EMPTY("empty.png"),
+	POINT("point.png"),
+	KEY("key.png");
+
+	private final String Image;
+	
+	TileType(String Image){
+		this.Image = Image;
+	}
+
+	public String GetImage() { 
+		return Image;
+	}
+
+	public boolean Accessible() {
+		return this != WALL;
+	}
+}
