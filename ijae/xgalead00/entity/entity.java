83e7e78cbfb4e79e7707d6f75a39d7d8244f6bc2
Dave Galea <davegalea27@gmail.com>
2025-12-26 17:21:12 +0100

    more work on entity class

--- ijae/xgalead00/entity/entity.java
+++ ijae/xgalead00/entity/entity.java
@@ -13,9 +13,9 @@ public abstract class Entity{
 	protected Direction direction = Direction.RIGHT; // starting direction
 
 	// Base image: right facing
-	protected BufferedImage[] BaseImage;
+	protected BufferedImage[] BaseImages;
 	// rotated imaages based on direction
-	protected BufferedImage[] RightImage, LeftImage, UpImage, DownImage
+	protected BufferedImage[] RightImages, LeftImages, UpImages, DownImages
 
 	// animation state
 	protected int ImageIndex = 0; //current image in animation
@@ -26,4 +26,34 @@ public abstract class Entity{
 	protected int TileWidth;
 	protected int TileHeight;
 
+
+	// Constructor
+	// initx: initial starting x coordinate
+	// inity: initial starting y coordinate
+	// BaseImage: right facing image 
+
+	public Entity(int initx, int inity, BufferedImage[] BaseImages, int TileWidth, int TileHeight) {
+		this.x = initx;
+		this.y = inity;
+		this.BaseImages = BaseImages;
+		this.TileWidth = TileWidth;
+		this.TileHeight = TileHeight;
+
+		//rotation of right facing images
+		RightImages = BaseImages;
+		DownImages = new BufferedImage[BaseImages.length];
+		LeftImages = new BufferedImage[BaseImages.length];
+		UpImages = new BufferedImage[BaseImages.length];
+
+		for (int i = 0; i < BaseImages.length; i++) {
+			DownImages[i] = RotateImage(BaseImages[i], 90);
+			LeftImages[i] = RotateImage(BaseImages[i], 180);
+			UpImages[i] = RotateImage(BaseImages[i], -90);
+		}
+	}
+
+	protected BufferedImage RotateImage(BufferedImage image, double angle) {
+		int ImageWidth = img.getWidth();
+		int Image
+	}
 }
