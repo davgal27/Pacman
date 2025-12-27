b51e858c37ff69051284f2efb579311577a4156d
Dave Galea <davegalea27@gmail.com>
2025-12-27 16:25:48 +0100

    player.java finished

---
+++ ijae/xgalead00/entity/player.java
@@ -0,0 +1,12 @@
+package ijae.xgalead00.entity;
+
+import javafx.scene.image.Image;
+
+// Player entity which uses entity logic. Direction handling set externally
+public class Player extends entity { 
+
+	public Player(int initx, int inity, Image[] BaseImages, int TileWidth, int TileHeight) {
+		super(initx, inity, BaseImages, TileWidth, TileHeight);
+	}
+
+}
