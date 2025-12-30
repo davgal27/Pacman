32b6f81cdf83f0b514dccf9eab80d467cdbfb5ba
Dave Galea <davegalea27@gmail.com>
2025-12-30 23:59:33 +0100

    more gui work

---
+++ ijae/xgalead00/gui/MenuView.java
@@ -0,0 +1,27 @@
+package ijae.xgalead00.gui;
+
+import ijae.xgalead00.game;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.Slider;
+import javafx.scene.layout.HBox;
+
+//Menu bar 
+public class MenuView extends HBox {
+
+    public MenuView(Game game) {
+
+        Slider speedSlider = new Slider(50, 500, 200);
+        speedSlider.setShowTickLabels(true);
+
+        speedSlider.valueProperty().addListener((obs, oldVal, newVal) ->
+            game.setGameSpeed(newVal.intValue())
+        );
+
+        Button level1 = new Button("Level 1");
+        level1.setOnAction(e -> game.loadLevel("levels/level1.txt"));
+
+        getChildren().addAll(level1, speedSlider);
+        setSpacing(10);
+    }
+}
