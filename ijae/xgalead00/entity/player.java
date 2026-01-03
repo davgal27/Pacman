5ab57c7c1915ef0f7d5b46fd488e447942d9537b
Dave Galea <davegalea27@gmail.com>
2026-01-03 13:42:46 +0100

    update to player to handle tile events

--- ijae/xgalead00/entity/player.java
+++ ijae/xgalead00/entity/player.java
@@ -4,10 +4,45 @@ import javafx.scene.image.Image;
 import ijae.xgalead00.Assets;
 
 // Player entity which uses entity logic. Direction handling set externally
-public class Player extends Entity { 
+public class Player extends Entity {
+	
+	private boolean alive = true;
+	private int score = 0; 
+	private boolean haskey = false;
 
 	public Player(int initx, int inity, Image[] BaseImages) {
 		super(initx, inity, Assets.PACMAN_FRAMES);
+	}
+
+	// Called by Game.java during update to handle different events with different tiles 
+	public void TileEvents (Tiles[][] tiles) {
+		Tiles tile = tiles[y][x];
+
+		if (tile == Tiles.POINT) {
+			tiles[y][x] = Tiles.EMPTY;
+			score += 10;
+		}
+
+		if (tile == tiles.KEY) {
+			tiles[y][x] = Tiles.EMPTY;
+			haskey = true;
+		}
+
+		if (tile == tiles.GATE && haskey = true) {
+			tiles[y][x] = Tiles.EMPTY;
+			Javafx overlay message you win!
+
+		}
+	}
+
+	public void die() {
+		alive = false;
+	}
+	public int getScore() {
+		return score;
+	}
+	public int isAlive() {
+		return alive
 	}
 
 }
